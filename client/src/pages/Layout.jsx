9dd524cf7b3bfb01c53459e0c77fea6af511a47a
laxman-goud <codewithlaxman92@gmail.com>
2025-11-18 11:16:19 +0530

    client(feat): add outlet to layout component

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
     return (
-        <div>Layout</div>
+        <div>
+            <h1>Layout</h1>
+            <div>
+                <Outlet />
+            </div>
+        </div>
     )
 }
 
