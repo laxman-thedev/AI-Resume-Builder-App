fd493bf2fcf790499ee9728e8d841ef5b7927f8a
laxman-goud <codewithlaxman92@gmail.com>
2025-11-23 15:24:34 +0530

    client(style): add edit, delete UI in dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -30,6 +30,17 @@ const Dashboard = () => {
         navigate('/app/builder/res123')
     }
 
+    const editTitle = async (e) => {
+        e.preventDefault()
+    }
+
+    const deleteResume = async (resumeId) => {
+        const confirm = window.confirm('Are you sure you want to delete this resume?')
+        if (confirm) {
+            setAllResumes(prev => prev.filter(resume => resume._id !== resumeId))
+        }
+    }
+
     useEffect(() => {
         loadAllResumes()
     }, [])
@@ -62,9 +73,9 @@ const Dashboard = () => {
                                 <p className='absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center' style={{ color: baseColor + '90' }}>
                                     Updated on {new Date(resume.updatedAt).toLocaleDateString()}
                                 </p>
-                                <div className='absolute top-1 right-1 group-hover:flex items-center hidden'>
-                                    <TrashIcon className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors' />
-                                    <PencilIcon className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors' />
+                                <div onClick={(e)=>{e.stopPropagation()}} className='absolute top-1 right-1 group-hover:flex items-center hidden'>
+                                    <TrashIcon onClick={()=>{deleteResume(resume._id)}} className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors' />
+                                    <PencilIcon onClick={()=>{setEditResumeId(resume._id); setTitle(resume.title)}} className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors' />
                                 </div>
 
                             </button>
@@ -165,6 +176,47 @@ const Dashboard = () => {
                                     className="w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors"
                                 >
                                     Upload Resume
+                                </button>
+                            </form>
+                        </div>
+                    )
+                }
+
+                {
+                    editResumeId && (
+                        <div
+                            className="fixed inset-0 bg-black/70 backdrop-blur flex justify-center items-center z-10"
+                            onClick={() => setEditResumeId('')}
+                        >
+                            <form
+                                onSubmit={editTitle}
+                                onClick={(e) => e.stopPropagation()}
+                                className="relative bg-slate-50 shadow-md rounded-lg w-full max-w-sm p-6"
+                            >
+                                <XIcon
+                                    className="absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer"
+                                    onClick={() => {
+                                        setEditResumeId('');
+                                        setTitle("");
+                                    }}
+                                />
+
+                                <h2 className="text-xl font-bold mb-4">Edit Resume Title</h2>
+
+                                <input
+                                    type="text"
+                                    className="w-full px-4 py-2 mb-4 border focus:border-green-600 focus:ring-1 focus:ring-green-600 rounded"
+                                    placeholder="Enter resume title"
+                                    required
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                />
+
+                                <button
+                                    type="submit"
+                                    className="w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors"
+                                >
+                                    Update
                                 </button>
                             </form>
                         </div>
