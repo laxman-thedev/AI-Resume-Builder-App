7560ca3a0d1df0b4ba79d2ded381c9a632cbd567
laxman-goud <codewithlaxman92@gmail.com>
2025-11-28 10:30:00 +0530

    client(style): add styles to right panel

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -56,7 +56,7 @@ const ResumeBuilder = () => {
                 </Link>
             </div>
 
-            <div className='max-w-7xl mx-auto px-4 pb-8'>
+            <div className="max-w-7xl mx-auto px-4 pb-8 grid grid-cols-1 lg:grid-cols-12 gap-6">
                 {/* Left Panel Form */}
                 <div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
                     <div className='bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1'>
@@ -64,7 +64,7 @@ const ResumeBuilder = () => {
                         {/* progress bar using activeSectionIndex */}
                         <hr className='absolute top-0 left-0 right-0 border-2 border-gray-200' />
                         <hr
-                            className='absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-200'
+                            className='absolute top-0 left-0 h-1 bg-linear-to-r from-green-500 to-green-600 border-none transition-all duration-200'
                             style={{ width: `${activeSectionIndex * 100 / (sections.length - 1)}%` }}
                         />
 
@@ -117,7 +117,14 @@ const ResumeBuilder = () => {
                 </div>
 
                 {/* Right Panel Form */}
-                <div></div>
+                <div className='lg:col-span-7 max-lg:mt-6'>
+                    <div>
+                        {/* ----- buttons ----- */}
+                    </div>
+
+                    {/* --- resume preview --- */}
+                    
+                </div>
             </div>
         </div>
 
