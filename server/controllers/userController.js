67b9cd812f51b9f2b90b02c285a8472eac3acba9
laxman-goud <codewithlaxman92@gmail.com>
2025-12-02 20:54:37 +0530

    server(feat):  add getUserById function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -60,3 +60,22 @@ export const loginUser = async (req, res) => {
         return res.status(400).json({ message: error.message });
     }
 }
+
+// controller for getting user by id
+// GET: /api/users/data
+export const getUserById = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        user.password = undefined;
+
+        return res.status(200).json({ user });
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
