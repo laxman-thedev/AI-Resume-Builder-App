c67e67fb7b192d6c6a2034c2d90b61406bffda79
laxman-goud <codewithlaxman92@gmail.com>
2025-12-03 19:24:59 +0530

    client(feat): add Toaster to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Login from './pages/Login'
 import { useDispatch } from 'react-redux'
 import api from './configs/api.js'
 import { login, setLoading } from './features/authSlice.js'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
 
@@ -39,6 +40,7 @@ const App = () => {
 
   return (
     <>
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="app" element={<Layout />}>
