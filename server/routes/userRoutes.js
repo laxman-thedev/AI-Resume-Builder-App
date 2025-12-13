12ec23a0ad893daffdc4ba1f8347f6458e7f84f8
laxman-goud <codewithlaxman92@gmail.com>
2025-12-13 10:30:00 +0530

    server(docs): add inline comments to userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,42 @@
 import express from "express";
-import { getUserById, getUserResumes, loginUser, registerUser } from "../controllers/userController.js";
+import {
+    getUserById,
+    getUserResumes,
+    loginUser,
+    registerUser
+} from "../controllers/userController.js";
 import protect from "../middlewares/authMiddleware.js";
 
+/*
+|--------------------------------------------------------------------------
+| User Routes
+|--------------------------------------------------------------------------
+| Handles authentication and user-related data
+*/
 const userRouter = express.Router();
 
+/*
+| Register a new user
+| POST /api/users/register
+*/
 userRouter.post("/register", registerUser);
+
+/*
+| Login user
+| POST /api/users/login
+*/
 userRouter.post("/login", loginUser);
 
+/*
+| Get logged-in user data
+| GET /api/users/data
+*/
 userRouter.get("/data", protect, getUserById);
+
+/*
+| Get all resumes of logged-in user
+| GET /api/users/resumes
+*/
 userRouter.get("/resumes", protect, getUserResumes);
 
 export default userRouter;
