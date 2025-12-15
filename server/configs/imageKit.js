fb06793c1a64826a82d4631ffa136f05eda9aa35
laxman-goud <codewithlaxman92@gmail.com>
2025-12-15 10:06:58 +0530

    server(docs): add inline comments to imageKit config file

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,5 +1,11 @@
 import ImageKit from '@imagekit/nodejs';
 
+/*
+|--------------------------------------------------------------------------
+| ImageKit Configuration
+|--------------------------------------------------------------------------
+| Handles image uploads for resume profile photos
+*/
 const imageKit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
