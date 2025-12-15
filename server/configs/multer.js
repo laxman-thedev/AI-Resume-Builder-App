48041cde0335185d7b8df8edad010a7b896607a1
laxman-goud <codewithlaxman92@gmail.com>
2025-12-15 10:07:11 +0530

    server(docs): add inline comments to multer config file

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -1,8 +1,14 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-});
+/*
+|--------------------------------------------------------------------------
+| Multer Configuration
+|--------------------------------------------------------------------------
+| Handles file uploads (used for resume images)
+| Stored temporarily on disk before processing
+*/
+const storage = multer.diskStorage({});
 
 const upload = multer({ storage });
 
-export default upload; 
+export default upload;
