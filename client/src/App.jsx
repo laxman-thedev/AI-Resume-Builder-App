112c8880eafe08c39b9695267f0d456218cc298d
laxman-goud <codewithlaxman92@gmail.com>
2025-12-17 10:30:00 +0530

    client(docs): add inline documentation to main, app, index.css files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,47 @@
 import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { Toaster } from 'react-hot-toast'
+
 import Home from './pages/Home'
 import Layout from './pages/Layout'
 import Dashboard from './pages/Dashboard'
 import ResumeBuilder from './pages/ResumeBuilder'
 import Preview from './pages/Preview'
-import { useDispatch } from 'react-redux'
+
 import api from './configs/api.js'
 import { login, setLoading } from './features/authSlice.js'
-import {Toaster} from 'react-hot-toast'
 
+/**
+ * Root application component
+ * - Handles routing
+ * - Fetches logged-in user data on refresh
+ */
 const App = () => {
 
   const dispatch = useDispatch()
 
+  /**
+   * Fetch user data if token exists
+   * Keeps user logged in on page reload
+   */
   const getUserData = async () => {
     const token = localStorage.getItem('token')
     try {
       if (token) {
-        const {data} = await api.get('/api/users/data', {headers: {Authorization: token}})
-        if(data.user) {
-          dispatch(login({token, user: data.user}))
+        const { data } = await api.get(
+          '/api/users/data',
+          { headers: { Authorization: token } }
+        )
+
+        if (data.user) {
+          dispatch(login({ token, user: data.user }))
         }
-        dispatch(setLoading(false))
-      }
-      else{
-        dispatch(setLoading(false))
       }
     } catch (error) {
-        dispatch(setLoading(false))
-        console.log(error.message)
+      console.log(error.message)
+    } finally {
+      dispatch(setLoading(false))
     }
   }
 
@@ -39,13 +51,18 @@ const App = () => {
 
   return (
     <>
+      {/* Global toast notifications */}
       <Toaster />
+
+      {/* App routes */}
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="app" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="builder/:resumeId" element={<ResumeBuilder />} />
         </Route>
+
         <Route path="view/:resumeId" element={<Preview />} />
       </Routes>
     </>
